257ef940afb223f5ef6694a6da01705021573054
Gerald <geraldnettey30@gmail.com>
2023-02-16 21:59:39 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - print  exactly that piece of art is useful
+ * main - Entry point
  *
- * Return:1 (Success)
+ * Return: Always 1
  */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-
+}
