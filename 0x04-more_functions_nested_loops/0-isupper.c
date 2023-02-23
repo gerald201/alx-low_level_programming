d30ab6c7c0307b88c83babfaa6e42f7801f532f4
Gerald <geraldnettey30@gmail.com>
2023-02-23 06:20:34 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: 1 for upper case or 0 anything else
+*/
+int _isupper(int c)
+{
+
+
+ if (c >= 'A' && c <= 'Z')
+ {
+ return (1);
+ }
+ else
+{
+ return (0);
+}
+
+}
