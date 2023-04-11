ec2a9a5a1146b6c522d4a31c75c25906cac3c5df
Gerald <geraldnettey30@gmail.com>
2023-04-11 13:26:07 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * author: NerFLee
+ * author: Gerald
  * @filename: name of the file to read
  * @letters: number of letters it should read and print
  *
